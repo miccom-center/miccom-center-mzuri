f3c85e2ef567fbc4982819a158e376d92f584b1e
mhandlin <handlin@uchicago.edu>
2017-06-07 09:46:09 -0500

    banner and style updates
    Changed title of front page to banner style and made some minor style tweaks i don't remember anymore

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 /* My scripts for the MICCoM website */
 
 /* script for collapsing and expanding submenus in navbar */
+
+
+
+/* give all objects in a group the height of the tallest object */
+
+
+
+/* toggle button text Show Abstract / Hide Abstract*/
+$('.hidden').removeClass('hidden').hide();
+$('.toggle-text').click(function() {
+    $(this).find('span').each(function() { $(this).toggle(); });
+});
