2788f866476eb25d58d0ffcf507a1db2154c0dcc
mhandlin <handlin@uchicago.edu>
2017-06-28 15:03:10 -0500

    idk
    dont even know why im still updating this repository

---
+++ footer.php
@@ -0,0 +1,49 @@
+<?php
+echo
+'    <footer class="footer">
+		<div class="container">
+			<div class="row footer-blurb">
+				<div class="col-lg-12">
+					<p>MICCoM is supported by the U.S. Department of Energy, Office of Science, Office of Basic Energy Sciences</p>
+				<hr>
+				</div>			
+				
+				<div class="col-sm-4 col-lg-2 footer-logo logo-lg">
+					<a href="http://www.uchicago.edu"><img src="images/logo.rgb.reversed-2.png" class="img-responsive"/> </a>
+				</div>		
+				
+				<div class="col-sm-4 col-lg-2 footer-logo logo-sm">
+					<a href="http://www.anl.gov"><img src="images/Argonne_smaller_logo_300x300.png" class="img-responsive"/> </a>
+				</div>
+	
+				<div class="col-sm-4 col-lg-2 footer-logo logo-lg">
+					<a href="http://www.ucdavis.edu"><img src="images/ucdavislogo_white.png" class="img-responsive"/> </a>				
+				</div>
+	
+				<div class="col-sm-4 col-lg-2 footer-logo logo-sm">
+					<a href="http://www.northwestern.edu"><img src="images/nu_logo_white.png" class="img-responsive"/> </a>
+				</div>
+	
+				<div class="col-sm-4 col-lg-2 footer-logo logo-sm">
+					<a href="https://www.umich.edu"><img src="images/UM_white.png" class="img-responsive"/> </a>
+				</div>
+	
+				<div class="col-sm-4 col-lg-2 footer-logo logo-lg">
+					<a href="https://www.nd.edu"><img src="images/ndamelogo_white.png"  class="img-responsive"/> </a>
+				</div>	
+			</div>			
+			<!-- /.row -->
+		</div>
+		<!-- /.container -->
+	</footer>
+
+  <!-- jQuery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  
+  <!-- Bootstrap Core JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  
+  <!-- local scripts -->
+  <script src="js/scripts.js"></script>
+';
+?>
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,38 @@
 /* My scripts for the MICCoM website */
 
+/*
+  When jumping to an anchor link (webaddress#id - takes you partway down the page to where the unique id lives), this offsets by the size of the navigation bar so it doesn't cover up the top bit, like a section header  
+*/
+
+// the amount in pixels we want to offset by 
+var offset=80;
+  
+// The function actually applying the offset
+function offsetAnchor() {
+  if (location.hash.length !== 0) {
+    window.scrollTo(window.scrollX, window.scrollY - offset);
+  }
+}
+
+// selects <a> elements with href starting with # which are NOT in the navbar
+var jumpLink = 'a[href^="#"]:not(nav):not(nav *)'
+
+// Captures click events of all <a> elements described above
+$(document).on('click', jumpLink, function(event) {
+  // Click events are captured before hashchanges. 
+  // Timeout causes offsetAnchor to be called after the page jump.
+  window.setTimeout(function() {
+    offsetAnchor();
+  }, 0);
+});
+
+// Set the offset when entering page with hash present in the url
+window.setTimeout(offsetAnchor, 0);
+
+
+
+
+
 /* script for collapsing and expanding submenus in navbar */
 
 
